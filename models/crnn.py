b6ace5e1b57bcbabe45f55f46eed5fadbae26025
Swall0w <technext.jpn@gmail.com>
2018-02-05 19:52:27 +0900

    [update]: fix model.

--- models/crnn.py
+++ models/crnn.py
@@ -25,14 +25,12 @@ class LeakyRelu(object):
 class BidirectionalLSTM(chainer.Chain):
     def __init__(self, nIn, nHidden, nOut):
         super(BidirectionalLSTM, self).__init__()
-        self.bi_lstm = L.NStepBiLSTM(n_layers=nHidden, in_size=nIn, out_size=nOut, dropout=.0)
+        self.bi_lstm = L.NStepBiLSTM(n_layers=1, in_size=nIn, out_size=nHidden, dropout=.0)
         self.embedding = L.Linear(nHidden*2, nOut)
 
     def __call__(self, x):
-        _, _, recurrent = self.bi_lstm(hx=None, cx=None, xs=[x])
-        print(recurrent)
-        for index, y in enumerate(recurrent):
-            print(index, '  ', y.data.shape)
+        _, _, recurrent = self.bi_lstm(hx=None, cx=None, xs=[item for item in x])
+        recurrent = F.stack(recurrent, axis=0)
         T, b, h = recurrent.data.shape
         t_rec = F.reshape(recurrent, (T * b, h))
         output = self.embedding(t_rec)
@@ -89,22 +87,20 @@ class CRNN(chainer.Chain):
     def __call__(self, x):
         features = self.cnn(x)
         b, c, h, w = features.data.shape
-        print('conv shape: ', features.data.shape)
         assert h ==1, 'the height of conv must be 1'
         features = F.squeeze(features, axis=2)
         features = F.transpose(features, axes=(2, 0, 1))
         output = self.rnn(features)
-#        output = features
         return output
 
 
 if __name__ == '__main__':
     import numpy as np
     from chainer import Variable
-    #, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+    # imgH, nc, nclass, nh
     crnn =  CRNN(32, 1, 37, 256)
     img = np.random.randn(1, 32, 100).astype(np.float32)
     img = Variable(np.expand_dims(img, axis=0))
     print('image shape: ',img.data.shape)
     pred = crnn(img)
-    print('pred shape: ', pred.data)
+    print('pred shape: ', pred.data.shape)
