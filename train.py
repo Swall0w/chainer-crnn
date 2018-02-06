e26727fb647a6c42bd80e957eeb2e3674d732f01
Swall0w <technext.jpn@gmail.com>
2018-02-06 23:52:38 +0900

    [add]: temporal updater.

--- train.py
+++ train.py
@@ -131,6 +131,53 @@ class TextImageDataset(chainer.dataset.DatasetMixin):
 
 
 # loss = F.connectionist_temporal_classification(y_batch, t_batch, BLANK, x_length_batch, t_length_batch)
+def yolo_converter(batch, device=None):
+    if len(batch) == 0:
+        raise ValueError('batch is empty')
+
+    batch = format_image_size.batch(batch)
+
+    first_elem = batch[0]
+    if isinstance(first_elem, tuple):
+        x = _to_device(device, _concat_arrays([item[0] for item in batch]))
+        x = Variable(x)
+        label = [item[1] for item in batch]
+        return x, label
+
+
+class CRNNUpdater(training.StandardUpdater):
+    def __init__(self, iterator, optimizer, converter=convert.concat_examples,
+            device=None):
+        if isinstance(iterator, iterator_module.Iterator):
+            iterator = {'main': iterator}
+        self._iterators = iterator
+
+        if not isinstance(optimizer, dict):
+            optimizer = {'main': optimizer}
+            self._optimizers = optimizer
+
+        if device is not None and device >= 0:
+            for optimizer in six.itervalues(self._optimizers):
+                optimizer.target.to_gpu(device)
+
+            self.converter = converter
+            self.loss_func = F.connectionist_temporal_classification
+            self.device = device
+            self.iteration = 0
+
+    def update_core(self):
+        batch = self._iterators['main'].next()
+        in_arrays = self.converter(batch, self.device)
+
+        optimizer = self._optimizers['main']
+        loss_func = self.loss_func or optimizer.target
+
+        if isinstance(in_arrays, tuple):
+            optimizer.update(loss_func, *in_arrays)
+        elif isinstance(in_arrays, dict):
+            optimizer.update(loss_func, **in_arrays)
+        else:
+            optimizer.update(loss_func, in_arrays)
 
 
 def main():
